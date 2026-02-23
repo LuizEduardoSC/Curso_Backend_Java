aab37f4042abaa472754138d5dd30eaf2a5b9f0b
LuizEduardoSC <luizeduardoedd1@gmail.com>
2026-02-23 12:03:49 -0300

    TarefaTestesParte2

--- test/br/com/leduardo/AllTests.java
+++ test/br/com/leduardo/AllTests.java
@@ -4,6 +4,6 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ ClienteServiceTest.class, ContratoServiceTest.class })
+@Suite.SuiteClasses({ ClienteServiceTest.class, ContratoServiceTest.class, TesteClienteTest.class })
 public class AllTests {
 }
